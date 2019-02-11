e292faebacbeb0fae48d71bbb074bd2683553203
John Morales <beljam@gmail.com>
2019-02-11 10:21:49 -0800

    bugfix
    Updated to add testing functions; fixed bug printing one or two coordinates as conditionals rather than coordinates.

--- battleship.py
+++ battleship.py
@@ -2,6 +2,11 @@
 # given a hit(x,y) func that returns bool
 # write a func that takes a gridsize and searches for a 3-unit ship
 
+def hit(x, y):
+    if y == 3 and x in [3, 4, 2]:
+        return True
+    return False
+
 def cruiser_search(gridsize):
     for x in range(1, gridsize + 1):
         indent = (x % 3) + 1
@@ -12,16 +17,20 @@ def cruiser_search(gridsize):
 
 def find_the_rest(gridsize, x, y):
     center = (x, y)
-    left = filter_hit_check(gridsize, x - 1, y)
-    right = filter_hit_check(gridsize, x + 1, y)
-    if left or right:
-        if left:
-            return [left, center, right] if right else [(x-2, y), left center]
+    left = (x - 1, y)
+    left_check = filter_hit_check(gridsize, left[0], left[1])
+    right = (x + 1, y)
+    right_check = filter_hit_check(gridsize, right[0], right[1])
+    if left_check or right_check:
+        if left_check:
+            return [left, center, right] if right else [(x-2, y), left, center]
         return [center, right, (x + 2, y)]
-    up = filter_hit_check(gridsize, x, y - 1)
-    down = filter_hit_check(gridsize, x, y + 1)
-    if up:
-        return [up, center, down] if down else [(x, y-2), up, center]
+    up = (x, y-1)
+    up_check = filter_hit_check(gridsize, up[0], up[1])
+    down = (x, y+1)
+    down_check = filter_hit_check(gridsize, down[0], down[1])
+    if up_check:
+        return [up, center, down] if down_check else [(x, y-2), up, center]
     return [center, down, (x, y + 2)]
 
 def filter_hit_check(gridsize, x, y):
@@ -29,3 +38,5 @@ def filter_hit_check(gridsize, x, y):
     if x in legal and y in legal:
         return hit(x, y)
     return False
+
+print(cruiser_search(10))
